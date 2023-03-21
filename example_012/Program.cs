fbdb2420081041c42fffd9acb582bbefe9e22981
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-21 11:17:54 +0300

    example_012_method
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

---
+++ example_012/Program.cs
@@ -0,0 +1,56 @@
+﻿// Вид 1 (1 группа методов)
+
+void Method()
+{
+    Console.WriteLine("Автор ...");
+}
+
+
+// Вид 2 (2 группа методов)
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения");
+
+void Method21(
+    string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg: "Текст", count: 4);
+Method21(count: 4, msg: "Новый текст");
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine("Year: " + year);
+
+// Вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while(i < count)
+{
+result = result + text;
+i++;
+}
+return result;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
