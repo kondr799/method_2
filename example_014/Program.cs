914a88d8c7f7079cf8e86a1b053d830773a3a9b4
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-21 18:35:05 +0300

    example_014_table
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

---
+++ example_014/Program.cs
@@ -0,0 +1,23 @@
+﻿// table
+
+for (int i = 2; i <= 10; i++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = count - 1; i >= 0; i--)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
