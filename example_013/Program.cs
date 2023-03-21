39c5e83e7935ecfd2b37bbf38cc2f895c06c999f
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-21 17:31:05 +0300

    example_013_for
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

---
+++ example_013/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = count - 1; i >= 0; i--)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
